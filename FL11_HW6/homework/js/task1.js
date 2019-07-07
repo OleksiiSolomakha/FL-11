4da06878e2f30fa3552880fdf0bd3da8f1be0f7e
Oleksii Solomakha <49649281+OleksiiSolomakha@users.noreply.github.com>
2019-07-07 14:41:34 +0300

    homework6

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,18 +1,18 @@
+let half = 2;
 let A = [];
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < half; i++) {
     A.push(+prompt('Coordinates for point A'));
 }
 let B = [];
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < half; i++) {
     B.push(+prompt('Coordinates for point B'));
 }
 let C = [];
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < half; i++) {
     C.push(+prompt('Coordinates for point C'));
 }
-
-let axisX = (A[0] + B[0]) / 2;
-let axisY = (A[1] + B[1]) / 2;
+let axisX = (A[0] + B[0]) / half;
+let axisY = (A[1] + B[1]) / half;
 
 if (C[0] === axisX && C[1] === axisY){
     console.log(true);    
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,9 +1,16 @@
-var age = window.prompt("How are you doing today?");
-
-console.log(age);
-
-var a, b;
-
-if ( a + c) {
-    console.log(c);
+let side = {
+    'a': +prompt('Triangle side A lenght'),
+    'b': +prompt('Triangle side B lenght'),
+    'c': +prompt('Triangle side C lenght')
+}
+if (isNaN(side.a) || isNaN(side.b) || isNaN(side.c)) {
+    alert('Please write numbers');
+} else if (side.a + side.b <= side.c || side.a + side.c <= side.b || side.b + side.c <= side.a) {
+    console.log('Triangle doesn’t exist');    
+} else if (side.a===side.b && side.b===side.c) {
+    console.log('Equivalent triangle');  
+} else if (side.a===side.b || side.b===side.c) {
+    console.log('Isosceles triangle'); 
+} else {
+    console.log('Normal traingle');
 }
